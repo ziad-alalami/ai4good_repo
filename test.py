2d671314f2390bbe373297064f7ee82817038dae
theivze <heroicgamesmc@gmail.com>
2025-10-21 22:09:49 +0300

    Updated test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from model.bert.inference import Classifier
 import time
 
-classifier = Classifier("model/bert/runs/1760524062/best_model.pt")
+classifier = Classifier("data/models/best_model.pt")
 
 start_time = time.time()
-pred, probs = classifier.predict("data/torgo_data/non_dysarthria_male/MC01_Session1_0005.wav", gender="male")
+pred, probs = classifier.predict("data/torgo_data/dysarthria_male/M01_Session1_0005.wav", gender="male")
 # pred, probs = classifier.predict("data/Recording.m4a", gender="male")
 end_time = time.time()
 
